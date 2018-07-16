22d46de5fab8f2223b5c4e779cb3ac4e12f3c810
malachilee <malachilee6@gmail.com>
2018-07-16 07:19:35 -0400

    first commit

---
+++ TechnologyReviewSite/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TechnologyReviewSite.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Brand { get; set; }
+        [DisplayName("Model Number")]
+        public string ModelNumber { get; set; }
+        public double Price { get; set; }
+        public virtual ICollection<Review>Reviews { get; set; }
+
+
+    }
+}
